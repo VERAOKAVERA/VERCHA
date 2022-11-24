cbca2f8e6eaf55538e7984500307dc87f153f659
veraokavera <veraokavera@gmail.com>
2022-11-24 09:13:07 +0300

    init project

---
+++ Vercha/Vercha/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Vercha
+//
+//  Created by Vlad on 24/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
